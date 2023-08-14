15d8e27fc80b8c1276023c98bc5e27daae4c2aa6
AlexRazor <alexrazor1337@gmail.com>
2023-08-14 17:39:59 +0300

    Print url on start

--- server.js
+++ server.js
@@ -10,15 +10,8 @@ export const app = express();
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'public')));
 
-const expressServer = app.listen(process.env.PORT || 3000);
+const expressServer = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT || 3000}/index.html`);
+});
 
 export const io = new Server(expressServer);
-
-io.on('connection', (socket) => {
-    console.log('New connection', socket.id);
-    
-    socket.on('event', (data) => {
-        console.log(data);
-        io.emit('message', data);
-    });
-});

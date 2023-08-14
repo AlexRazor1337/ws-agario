6baf8be590938295f87eb79a66d7a8f9917c56fa
AlexRazor <alexrazor1337@gmail.com>
2023-08-15 00:42:20 +0300

    Socket admin panel

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
+import path from 'path';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import { Server } from "socket.io";
-import path from 'path';
+import { instrument } from '@socket.io/admin-ui';
+
+const __dirname = path.resolve();
 
 export const app = express();
 
-const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'public')));
 
 const expressServer = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT || 3000}/index.html`);
+    console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
 });
 
-export const io = new Server(expressServer);
+export const io = new Server(expressServer, {
+    cors: {
+        origin: [`http://localhost:${process.env.ADMIN_PORT}`],
+        credentials: true,
+    }
+});
+
+if (process.env.NODE_ENV === 'development') {
+    const hashedPassword = bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10);
+    instrument(io, {
+        auth: {
+            type: "basic",
+            username: "admin",
+            password: hashedPassword
+        },
+        mode: "development",
+    });
+
+    const adminApp = express();
+    adminApp.use(express.static('./node_modules/@socket.io/admin-ui/ui/dist'));
+    adminApp.listen(process.env.ADMIN_PORT, () => {
+        console.log(`Admin UI is running on http://localhost:${process.env.ADMIN_PORT}`);
+    });
+}
